2fef9459436dec0439dbf3c6a34c1d5b22a330df
Eddie Molina <Eddiestcc@gmail.com>
2024-03-03 16:10:22 -0500

    adding all project files

---
+++ 03-reviews/setup/Review/app.js
@@ -0,0 +1,27 @@
+// DOM Selectors
+let card = document.getElementById("card");
+
+let userList = [{
+    picture: "-",
+    name: "Susan Smith",
+    occupation: "Web Developer",
+    review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu elit fringilla odio auctor malesuada. Donec non tortor laoreet, dictum mi at, euismod neque. Praesent sagittis lectus risus, non semper dui placerat nec. Cras molestie mauris fringilla neque venenatis fringilla. Aenean a urna neque. Aenean posuere porttitor libero ut ultricies. Nulla in risus sit amet risus rhoncus scelerisque quis vitae magna. Ut suscipit porta tellus. Nunc mollis, ligula eu faucibus consectetur, ex erat elementum libero, et eleifend lacus elit non enim. Pellentesque venenatis odio lorem, ac semper nibh pulvinar at. Cras eleifend sem eget pellentesque congue.",
+},
+{
+    picture: "-",
+    name: "John Doe",
+    occupation: "Engineer",
+    review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu elit fringilla odio auctor malesuada. Donec non tortor laoreet, dictum mi at, euismod neque. Praesent sagittis lectus risus, non semper dui placerat nec. Cras molestie mauris fringilla neque venenatis fringilla. Aenean a urna neque. Aenean posuere porttitor libero ut ultricies. Nulla in risus sit amet risus rhoncus scelerisque quis vitae magna. Ut suscipit porta tellus. Nunc mollis, ligula eu faucibus consectetur, ex erat elementum libero, et eleifend lacus elit non enim. Pellentesque venenatis odio lorem, ac semper nibh pulvinar at. Cras eleifend sem eget pellentesque congue.",
+},
+{
+    picture: "-",
+    name: "Joe Smith",
+    occupation: "Industrial Designer",
+    review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu elit fringilla odio auctor malesuada. Donec non tortor laoreet, dictum mi at, euismod neque. Praesent sagittis lectus risus, non semper dui placerat nec. Cras molestie mauris fringilla neque venenatis fringilla. Aenean a urna neque. Aenean posuere porttitor libero ut ultricies. Nulla in risus sit amet risus rhoncus scelerisque quis vitae magna. Ut suscipit porta tellus. Nunc mollis, ligula eu faucibus consectetur, ex erat elementum libero, et eleifend lacus elit non enim. Pellentesque venenatis odio lorem, ac semper nibh pulvinar at. Cras eleifend sem eget pellentesque congue.",
+},
+{
+    picture: "-",
+    name: "Jane Doe",
+    occupation: "Nurse",
+    review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu elit fringilla odio auctor malesuada. Donec non tortor laoreet, dictum mi at, euismod neque. Praesent sagittis lectus risus, non semper dui placerat nec. Cras molestie mauris fringilla neque venenatis fringilla. Aenean a urna neque. Aenean posuere porttitor libero ut ultricies. Nulla in risus sit amet risus rhoncus scelerisque quis vitae magna. Ut suscipit porta tellus. Nunc mollis, ligula eu faucibus consectetur, ex erat elementum libero, et eleifend lacus elit non enim. Pellentesque venenatis odio lorem, ac semper nibh pulvinar at. Cras eleifend sem eget pellentesque congue.",
+}]
