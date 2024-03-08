3c6a18482195b2ac9b747d30993cd5b1a49ed075
Eddie Molina <Eddiestcc@gmail.com>
2024-03-07 21:43:01 -0500

    adding fuctionality to faq page

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,13 @@
 //using selectors inside the element
 // traversing the dom
+// Dom selectors
+let buttons = document.querySelectorAll(".question-btn");
+
+// functions
+
+let selectBtn = () => {
+    buttons.forEach(i => {
+        console.log("click", [i]);
+    });
+};
+
