139a95c5c12eb66cec2609b484876f9049cb3519
Eddie Molina <Eddiestcc@gmail.com>
2024-03-13 17:08:30 -0400

    final changes to FAQ page

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -2,79 +2,88 @@
 // traversing the dom
 // Dom selectors
 let buttons = document.querySelectorAll(".question-btn");
-let buttonStyle = document.querySelectorAll(".fa-plus");
+let buttonStyle = document.querySelectorAll(".fa-plus-square");
 let questText = document.querySelectorAll(".question-text");
 
+// variables 
 
-// Buttons Functions
+let buttonSwitch = 0;
 
-let getBtnOne = () => {
-    questText[0].classList.toggle("question-text");
-    questText[0] = true;
-    questText[1] = false;
-    questText[2] = false;
-
-    if (questText[1] || questText[2] === false) {
-        questText[1].classList.add("question-text");
-        questText[2].classList.add("question-text");
-    }
-
-   buttonStyle[0].classList.toggle("fa-plus");
-   buttonStyle[0].classList.toggle("fa-minus");
-   buttonStyle[0] = true;
-   buttonStyle[1] = false;
-   buttonStyle[2] = false;
-
-   if (buttonStyle[1] || buttonStyle[2] === false) {
-    buttonStyle[1].classList.add("fa-plus");
-    buttonStyle[2].classList.add("fa-plus");
+// button style toggle function 
+let buttonToggle = () => {
+    if (buttonSwitch === 1) {
+        buttonStyle[0].classList.toggle("fa-plus-square");
+        buttonStyle[0].classList.toggle("fa-minus-square");
+        buttonStyle[1].classList.remove("fa-minus-square");
+        buttonStyle[1].classList.add("fa-plus-square");
+        buttonStyle[2].classList.remove("fa-minus-square");
+        buttonStyle[2].classList.add("fa-plus-square");
+    } else if (buttonSwitch === 2) {
+        buttonStyle[1].classList.toggle("fa-plus-square");
+        buttonStyle[1].classList.toggle("fa-minus-square");
+        buttonStyle[0].classList.remove("fa-minus-square");
+        buttonStyle[0].classList.add("fa-plus-square");
+        buttonStyle[2].classList.remove("fa-minus-square");
+        buttonStyle[2].classList.add("fa-plus-square");
+    } else if (buttonSwitch === 3) {
+        buttonStyle[2].classList.toggle("fa-plus-square");
+        buttonStyle[2].classList.toggle("fa-minus-square");
+        buttonStyle[0].classList.remove("fa-minus-square");
+        buttonStyle[0].classList.add("fa-plus-square");
+        buttonStyle[1].classList.remove("fa-minus-square");
+        buttonStyle[1].classList.add("fa-plus-square");
     }
 };
+
+ // Buttons Functions
+// button 1
+let getBtnOne = () => {
+// quetions answer toggle 
+    questText[0].classList.toggle("hide");
+    questText[0] = false;
+    questText[1] = true;
+    questText[2] = true;
+
+    if (questText[1] || questText[2] === true) {
+        questText[1].classList.add("hide");
+        questText[2].classList.add("hide");
+    }
+// button style toggle
+    buttonSwitch = 1;
+    buttonToggle();
+};
+// button 2
 let getBtnTwo = () => {
-    questText[1].classList.toggle("question-text");
+// quetions answer toggle 
+    questText[1].classList.toggle("hide");
     questText[0] = false;
     questText[1] = true;
     questText[2] = false;
 
     if (questText[0] || questText[2] === false) {
-        questText[0].classList.add("question-text");
-        questText[2].classList.add("question-text");
+        questText[0].classList.add("hide");
+        questText[2].classList.add("hide");
     }
-
-    buttonStyle[1].classList.toggle("fa-plus");
-    buttonStyle[1].classList.toggle("fa-minus");
-    buttonStyle[1] = true;
-    buttonStyle[0] = false;
-    buttonStyle[2] = false;
-
-   if (buttonStyle[0] || buttonStyle[2] === false) {
-    buttonStyle[0].classList.add("fa-plus");
-    buttonStyle[2].classList.add("fa-plus");
-   }
+// button style toggle 
+    buttonSwitch = 2;
+    buttonToggle();
 };
+// button 3 
 let getBtnThree = () => {
-    questText[2].classList.toggle("question-text");
+// quetions answer toggle     
+    questText[2].classList.toggle("hide");
     questText[0] = false;
     questText[1] = false;
     questText[2] = true;
 
     if (questText[0] || questText[1] === false) {
-        questText[0].classList.add("question-text");
-        questText[1].classList.add("question-text");
+        questText[0].classList.add("hide");
+        questText[1].classList.add("hide");
     }
-    
-    buttonStyle[2].classList.toggle("fa-plus");
-    buttonStyle[2].classList.toggle("fa-minus");
-    buttonStyle[2] = true;
-    buttonStyle[0] = false;
-    buttonStyle[1] = false;
-
-   if (buttonStyle[0] || buttonStyle[1] === false) {
-    buttonStyle[0].classList.add("fa-plus");
-    buttonStyle[1].classList.add("fa-plus");
-   }
+// button style toggle 
+    buttonSwitch = 3;
+    buttonToggle();
 };
-
 // Button Clicks 
 
 buttons[0].addEventListener("click", getBtnOne);
