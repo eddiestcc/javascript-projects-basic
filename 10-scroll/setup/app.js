b7ba2847e975315b171712c094c052d21fc77a54
Eddie Molina <Eddiestcc@gmail.com>
2024-03-27 13:38:31 -0400

    adding html changes and css properties for functionality as well as new js functions

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -11,3 +11,58 @@
 
 // ********** smooth scroll ************
 // select links
+const topLink = document.querySelector(".top-link");
+const hamburger = document.querySelector(".fa-bars");
+const links = document.querySelector(".links");
+const navbar = document.querySelector(".fixed-nav");
+const clearBtn = document.querySelector(".clear-btn");
+
+// SHOW TOP LINK FUNCTION 
+let showTopLink = () => {
+    topLink.classList.add("show-link");
+}
+// HIDE TOP LINK FUNCTION 
+let hideTopLink = () => {
+    topLink.classList.remove("show-link");
+}
+// SHOW LINKS MENU FUNCTION 
+let showLinksMenu = () => {
+    links.classList.remove("links-hide");
+}
+// HIDE LINKS MENU FUNCTION 
+let hideLinksMenu = () => {
+    links.classList.add("links-hide");
+}
+
+// SWITCH NAVBAR STYLES 
+let switchNavBar = () => {
+    if (clearBtn.getBoundingClientRect().y < 450) {
+        navbar.classList.remove("clear-nav");
+    } if (clearBtn.getBoundingClientRect().y > 451) {
+        navbar.classList.add("clear-nav");
+    }
+}
+
+// STICKY BUTTON TOGGLE APPEARANCE
+
+let toggleSticky = () => {
+    if (clearBtn.getBoundingClientRect().y < 15) {
+        topLink.classList.add("show-link");
+    } else if (topLink.getBoundingClientRect().y > 15) {
+        topLink.classList.remove("show-link");
+    }
+}
+
+// HAMBURGER MENU TOGGLE 
+
+let toggleMenu = () => {
+    links.classList.toggle("links-hide");
+}
+
+// DOM EVENT LISTENERS 
+window.addEventListener("scroll", () => {
+    switchNavBar();
+    toggleSticky();
+});
+
+hamburger.addEventListener("click", toggleMenu);
