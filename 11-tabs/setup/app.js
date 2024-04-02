c11916123adb40e915ab05b7e3f1cef1630c29ed
Eddie Molina <Eddiestcc@gmail.com>
2024-04-02 14:35:05 -0400

    uploading

--- 11-tabs/setup/app.js
+++ 11-tabs/setup/app.js
@@ -1,17 +1,43 @@
 // DOM Selectors 
 const buttons = document.querySelectorAll(".tab-btn");
-const paraName = document.querySelectorAll(".names");
-// Browse function 
+const content = document.querySelectorAll(".content");
 
-let browse = () => {
+// SWITCH FUNCTION 
+
+let switchSelection = () => {
     buttons.forEach(button => {
-        addEventListener ("click", (e)=> {
-            currentSel = e.currentTarget.value;
-            console.log(currentSel, button);
+        button.addEventListener("click", () => {
+            if (button.textContent === "goals") {
+                content[2].classList.add('active');
+                buttons[2].classList.add('active');
+                content[0].classList.remove('active');
+                buttons[0].classList.remove('active');
+                content[1].classList.remove('active');
+                buttons[1].classList.remove('active');
+            }
+            else if (button.textContent === "vision") {
+                content[1].classList.add('active');
+                buttons[1].classList.add('active');
+                content[0].classList.remove('active');
+                buttons[0].classList.remove('active');
+                content[2].classList.remove('active');
+                buttons[2].classList.remove('active');
+            } else if (button.textContent === "history") {
+                content[0].classList.add('active');
+                buttons[0].classList.add('active');
+                content[1].classList.remove('active');
+                buttons[1].classList.remove('active');
+                content[2].classList.remove('active');
+                buttons[2].classList.remove('active');
+            }
         })
-    }); 
+    });
 }
 
 // DOM EVENT LISTENERS 
+window.addEventListener("DOMContentLoaded", () => {
+    content[0].classList.add('active');
+    buttons[0].classList.add('active');
+})
 
-document.addEventListener("click", browse)
+switchSelection();
